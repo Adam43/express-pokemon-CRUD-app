155a7d480cd5f153c2c33b910f57810edf5bab78
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-02-08 21:33:52 -0500

    first commit

---
+++ createPokemon.js
@@ -0,0 +1,24 @@
+import express from "express"; //Import Express
+import { Low } from "lowdb"; //Import the LowDB module. Uses a JSON file to create our "database"
+import { JSONFile } from "lowdb/node";
+import createPokemon from "./createPokemon.js"
+import setUpPokemonRoutes from "./routes/routes.js";
+
+//line 27 await db.write()
+export default async function createPokemon(){
+    const adapter = new JSONFile("db.json");
+    const db = new Low(adapter);
+
+    await db.read();
+
+    db.data = db.data || { pokemons: [] };
+
+    await db.write();
+
+    const app = express();
+
+    app.use(express.json());
+    app.use("/pokemon", setUpPokemonRoutes(db))
+
+    return app;
+}
---
+++ routes/routes.js
@@ -0,0 +1,50 @@
+import express from "express";
+
+export default function setUpPokemonRoutes(db){
+    const router = express.Router();
+//Default
+    router.get("/", (request, response) => {
+        console.log("success");
+        
+        response.status(200).json({
+            success: true,
+            pokemons:db.data.pokemons
+        })
+    });
+//Get | One specific item
+    router.get("/", (request, response) => {
+        const pokemon = request.params.id
+        const currentPokemon = db.data.pokemon.find((element) => element.id === pokemon);
+
+        response.status(200).json({
+            success: true,
+            id: currentPokemon
+        })
+    });
+//Create
+    router.post("/", (request, response) => {
+       
+
+        db.data.pokemons.push({
+            id: nanoid(4),
+            name: request.body.todo,
+        })
+
+        response.status(201).json({
+            success: true,
+        })
+    });
+
+//Update    
+    router.put("/:id", (request, response) => {
+        console.log("success");
+    });
+//Delete
+    router.get("/:id", (request, response) => {
+        console.log("success");
+
+        db.data.pokemon.splice(pokemon)
+    });
+
+    return router 
+}       
